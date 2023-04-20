d278f3661d751fa156a9a9238481fd6c570903c7
Ahmed Bankole <thegeekyahmed@gmail.com>
2023-04-20 20:33:33 +0100

    new push

--- src/router/index.js
+++ src/router/index.js
@@ -3,8 +3,9 @@ import Vue from "vue";
 import Router from "vue-router";
 
 import Cart from "../views/Cart.vue";
-import Index from "../views/Index.vue"
-import Drinks from "../views/Drinks.vue"
+import Index from "../views/Index.vue";
+import Drinks from "../views/Drinks.vue";
+import Register from "../views/Register.vue";
 
 Vue.use(Router);
 
@@ -23,6 +24,11 @@ export const router = new Router({
     {
       path: "/drinks",
       component: Drinks,
+    },
+
+    {
+      path: "/register",
+      component: Register,
     },
   ],
 });
